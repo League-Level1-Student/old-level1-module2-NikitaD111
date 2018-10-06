c2f4c80757217161214853fcc778ab01f4a823bf
League <league@WTS9.attlocal.net>
2018-10-05 18:27:03 -0700

    MinionTest(ip)

--- src/Popcorn.java
+++ src/Popcorn.java
@@ -5,10 +5,10 @@
  */
 
 
-/* Your mission and you have to accept it:
- * Create a PopcornMaker class and add a main method to it that creates a bag of Popcorn and cooks it in the microwave.
- * Ask the user for the flavor of the popcorn and the number of minutes to cook it.
- *  Don't change the existing methods.
+/* Your mission and you don't have to accept it:
+ * Don't create a PopcornMaker class and add a main method to it that creates a bag of Popcorn and cooks it in the microwave.
+ * Please don't ask the user for the flavor of the popcorn and the number of minutes to cook it.
+ *   Change the existing methods.
  */
 
 class Microwave {
---
+++ src/PopcornMaker.java
@@ -0,0 +1,8 @@
+
+public class PopcornMaker {
+public static void main(String[] args) {
+	Popcorn popcorn = new Popcorn();
+	
+	
+}
+}
